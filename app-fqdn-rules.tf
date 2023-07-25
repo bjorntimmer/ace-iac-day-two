8c874368098b9a981bf13c68bf69fcbeb3f3fabf
Björn Timmer <bjorn@timmer.pro>
2023-07-25 11:47:45 +0200

    Add Ubuntu wildcard
    Add *.ubuntu.com to the filter. This will allow the following 2 commands to work:
    
    sudo apt-get update -y
    sudo apt-get upgrade -y

--- app-fqdn-rules.tf
+++ app-fqdn-rules.tf
@@ -5,6 +5,8 @@ locals {
       "*.aviatrix.com" = "443"
       "aviatrix.com"   = "80"
       "*.ubuntu.com"   = "80"
+      "*.quality.nl"   = "80"
+      "*.quality.nl"   = "443"
     }
     udp = {
       "dns.google.com" = "53"
